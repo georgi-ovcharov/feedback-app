2b9e418eb17ac002d06060aa82c49a3f9e8ff18c
Georgi Ovcharov <georgi_ovcharov@epam.com>
2022-05-27 17:46:33 +0300

    updated functionality

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -58,8 +58,11 @@ export const FeedbackProvider = ({ children }) => {
       },
       body: JSON.stringify(updItem)
     })
+
+    const data = await response.json();
+
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
   };
 
